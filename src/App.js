7d2d359add82724d75c4886107bd4828cd3124ae
Adam Erb <adamlewiserb@gmail.com>
2022-02-10 16:51:00 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  // Created the IP state
+  const [ip, setIP] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('')
+
+  // Created a function to get IP address from the API
+  const getData = async () => {
+    const res = await axios.get('https://geolocation-db.com/json/')
+    console.log(res.data);
+    setIP(res.data.IPv4)
+    setCity(res.data.city)
+    setState(res.data.state)
+  }
+  
+  useEffect( () => {
+    // Pass getData method to the lifecycle method
+    getData()
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello! You may not know me, but I know some things about you...</h2>
+      <h4>I know you are located in {city}, {state}.</h4>
+      <h4>And your IP address is: {ip}</h4>
     </div>
   );
 }
